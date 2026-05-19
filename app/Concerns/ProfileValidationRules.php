86e94b64c32e075984e6ab8f9f249f954611377e
ogecc <ojerotijevic@gmail.com>
2026-05-19 19:04:50 +0200

    Disable email verification, add DNS email validation on registration

--- app/Concerns/ProfileValidationRules.php
+++ app/Concerns/ProfileValidationRules.php
@@ -41,7 +41,7 @@ trait ProfileValidationRules
         return [
             'required',
             'string',
-            'email',
+            'email:rfc,dns',
             'max:255',
             $userId === null
                 ? Rule::unique(User::class)
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -15,7 +14,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
